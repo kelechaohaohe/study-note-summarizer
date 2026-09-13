e7aa9913cce4a6812a3d8b3bd66281cc1ef904b1
hooi0004 <hooi0004@student.monash.edu>
2026-09-13 19:04:13 +0800

    feat: add navigation for login, signup, homepage

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { useAuth } from "../context/AuthContext.jsx";
+
+export default function Navbar() {
+  const { user, logout } = useAuth();
+
+  return (
+    <nav className="navbar">
+      <Link to="/" className="navbar-brand">Study Notes Summarizer</Link>
+      <div className="navbar-actions">
+        {user ? (
+          <>
+            <span className="navbar-user">{user.email}</span>
+            <button type="button" className="navbar-link" onClick={logout}>
+              Log out
+            </button>
+          </>
+        ) : (
+          <>
+            <Link to="/login" className="navbar-link">Log in</Link>
+            <Link to="/signup" className="navbar-link navbar-cta">Sign up</Link>
+          </>
+        )}
+      </div>
+    </nav>
+  );
+}
